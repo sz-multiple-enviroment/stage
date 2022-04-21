2f0d75251aa0f598d72a840b0cc7ae57a4f5972a
simonezardi <83826210+simonezardi@users.noreply.github.com>
2022-04-21 11:38:34 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,11 +3,6 @@
 mkdir output
 
 pandoc -s main.md -o output/main.html
-pandoc main.md -o output/main_bare.html
-pandoc -s main.md -o output/main.pdf
-pandoc -s main.md -o output/main.epub
-pandoc -s main.md -o output/main.odt
-pandoc -s main.md -o output/main.docx
 
 date > output/.build_date.txt
 
