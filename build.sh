d1711fc54700c00b6c1e9f814bae6b1a8e152b10
simonezardi <83826210+simonezardi@users.noreply.github.com>
2022-04-21 11:28:49 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -12,3 +12,6 @@ pandoc -s main.md -o output/main.docx
 date > output/.build_date.txt
 
 echo "generated_at: $(date)" > variables.yml
+
+mustache variables.yml index.output.html > output/index.html
+mustache variables.yml README.output.md > output/README.md
